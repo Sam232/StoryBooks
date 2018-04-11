5d1633481a3e6eeae8480cbae4f4b6e5c27e6d92
Samuel <jread912@gmail.com>
2018-04-10 23:50:26 -0700

    Added Google-OAuth For User Authentication

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
 
+//Connect To Production MongoDB Database
 const {mongooseConnect} = require("./config/database");
+mongooseConnect(mongoose);
+
+//Passport Config
+require("./config/passport")(passport);
+
+//Load Routes
+const auth = require("./routes/auth");
 
 var app = express();
 
-mongooseConnect(mongoose);
+//Express Session Middleware
+app.use(session({
+  secret: "secret",
+  resave: true,
+  saveUninitialized: true 
+}));
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
   res.send("welcome");
 });
+
+//Express Routes
+app.use("/auth", auth);
 
 var PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => {
