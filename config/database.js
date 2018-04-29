c1d49acfd03f0d123d7da6fba675a7de47800fd7
Samuel <jread912@gmail.com>
2018-04-29 14:25:54 -0700

    Updated The Config And Package.json files

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongooseConnect: (mongoose) => {
-    mongoose.connect("mongodb://SamuelKobina:newPassword1@ds261929.mlab.com:61929/storybooks-production").then((res) => {
+    mongoose.connect(process.env.mongoDBURI).then((res) => {
       if(res){
         console.log("Connected To Production MongoDB Database Server");
       }
