77a4dcfb7696f100e4b927bd232942d4625bac3a
Samuel <jread912@gmail.com>
2018-04-29 15:28:32 -0700

    Added Two New Keys File

---
+++ config/keys/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID: "239404457199-6scb1pk7b1i7cntrco4m6pp6a58flsha.apps.googleusercontent.com",
+  googleClientSecret: "RHIjqXa_Hg7CxWtJG1SwUaKY",
+  mongoDBURI: "mongodb://SamuelKobina:newPassword1@ds261929.mlab.com:61929/storybooks-production"
+}
